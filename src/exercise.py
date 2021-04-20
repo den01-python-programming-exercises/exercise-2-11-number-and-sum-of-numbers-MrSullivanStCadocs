52de474d2a7f54e940805f6e28e30706fa1c2ed4
MrSullivan <>
2021-04-20 22:01:37 +0000

    while true and 2 if statements

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,21 @@
 def main():
     #write your code below this line
+    amountOfNumbers = 0
+    sumOfNumbers = 0
+
+    while True:
+      number = int(input("Give a number:"))
+
+      if (number == 0):
+        break
+      
+      if (number != 0):
+        amountOfNumbers = amountOfNumbers + 1
+        sumOfNumbers = sumOfNumbers + number
+    
+    print("Number of numbers: " + str(amountOfNumbers))
+    print("Sum of the numbers: " + str(sumOfNumbers))
+    
 
 if __name__ == '__main__':
     main()
